90befb6507d13a84ced72c820412af5a46d1344c
Matthew Fee <mfee21@gmail.com>
2025-10-20 06:59:11 -0600

    gemini working (#2)

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -1,9 +1,8 @@
 from google.adk.agents import Agent
-from google.adk.models.lite_llm import LiteLlm
 
 root_agent = Agent(
     name="greeting_agent",
-    model=LiteLlm(model='openai/gpt-5-mini-2025-08-07'),
+    model="gemini-2.5-flash",
     description="Greeting agent",
     instruction="""
     You are a helpful assistant that greets the user.
