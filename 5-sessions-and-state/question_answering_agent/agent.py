6a37d0273ee139d49ecbf46df21907b39cb6e22e
mfee21 <mfee21@gmail.com>
2025-10-30 22:53:17 -0600

    fixed by comparing to example code

--- 5-sessions-and-state/question_answering_agent/agent.py
+++ 5-sessions-and-state/question_answering_agent/agent.py
@@ -1,15 +1,17 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents import Agent
 
-root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='question_answering_agent',
-    description='A helpful assistant for user questions.',
+# Create the root agent
+question_answering_agent = Agent(
+    name="question_answering_agent",
+    model="gemini-2.5-flash",
+    description="Question answering agent",
     instruction="""
     You are a helpful assistant that answers questions about the user's preferences.
 
     Here is some information about the user:
+    Name: 
     {user_name}
-    Preferences:
+    Preferences: 
     {user_preferences}
     """,
 )
